4ac90801235488b8552ab939a33400385180acb1
Abel Lopes <abelflopes@gmail.com>
2023-09-21 04:22:18 +0100

    chore: use different tsconfig for builds

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,6 @@ import { sum } from "./index";
 
 describe("Sum", () => {
   it("should correctly return the sum of the provided numbers", () => {
-    expect(sum(1, 2, 3)).toBe(8);
+    expect(sum(1, 2, 3, 4)).toBe(10);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+/**
+ * Perform the sum of the provided numbers
+ * @param numbers - infinite list of numbers to sum
+ * @returns
+ */
+
 export const sum = (...numbers: number[]): number =>
-  numbers.reduce((prev, curr) => curr + prev);
+  numbers.reduce((previousValue, currentValue) => currentValue + previousValue);
